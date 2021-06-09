a55eb3992b36a7e41a5fb8441928511fb517ac2b
jyaacoub <50300488+jyaacoub@users.noreply.github.com>
2021-06-09 16:21:28 +0400

    typo

--- GUI/src/mainPagejs/models.js
+++ GUI/src/mainPagejs/models.js
@@ -566,7 +566,7 @@ document.querySelector('#modelArch .closePopupBtn').onclick = function(){
 
 function bootModelsTab(){
   // TODO: CHECK TO SEE IF THERE IS AN ACTIVE REQUEST TO VIEW ARCH.
-  // if aactive it will cause an issue with the arch not being viewed.
+  // If active it will cause an issue with the arch not being viewed.
   updateCharts();
 }
 
